524e208be385728d6d2541fe39a639bc3fa9c5eb
Kjartan Rekdal Müller <kjartan@muller.no>
2019-05-06 16:48:14 +0200

    Change config for now deployment

--- epubcheck.js
+++ epubcheck.js
@@ -6,7 +6,7 @@ epubchecker(path.join(epubPath, 'ebook.epub'), {
     includeWarnings: true,
     // do not check CSS and font files
     exclude: /\.(css|ttf|opf|woff|woff2)$/,
-    output: "./reports/epubcheck.json"
+    output: path.join(epubPath, 'epubcheck.json')
 }).then(value=>{
-    console.log(value);
+    console.log("Epub OK!");
 });
